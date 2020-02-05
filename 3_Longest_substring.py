3a3e4d4b30976ebae63167e15d3ada7850ef94e2
yuyuchen <qscez2001@gmail.com>
2020-02-05 16:19:24 +0800

    fix no.3

--- 3_Longest_substring.py
+++ 3_Longest_substring.py
@@ -6,6 +6,8 @@ def lengthOfLongestSubstring(self, s: str) -> int:
         for j in range(i+1, len(s)):
             if s[j] not in temp:
                 temp.append(s[j])
+            else:
+                break
         if len(temp) > maxlen:
             maxlen = len(temp)
     return maxlen
