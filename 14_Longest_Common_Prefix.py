160798371eae03d7a7d9af007ef86c00d4da9597
yuyuchen <qscez2001@gmail.com>
2020-02-13 13:33:56 +0800

    q14-16

---
+++ 14_Longest_Common_Prefix.py
@@ -0,0 +1,50 @@
+def longestCommonPrefix(strs):
+    
+    if not strs:
+        return ""
+
+    length = []
+    for str in strs:
+        length.append(len(str))
+    min_len = min(length)
+
+    if len(strs) == 1:
+        return strs[0]
+    
+    flag = 1
+    output = ""
+    for j in range(min_len):
+        num = 0
+        for i in range(len(strs)-1):
+            # print(strs[i][j], strs[i+1][j])
+            if strs[i][j] == strs[i+1][j] and flag:
+                num += 1
+            else:
+                flag = 0
+        if num == len(strs)-1:
+            output = output + strs[i][j]
+        # print(num)
+
+    return output
+
+# others solution
+def longestCommonPrefix(self, strs):
+    """
+    :type strs: List[str]
+    :rtype: str
+    """
+    if not strs: return ""
+    if len(strs) == 1: return strs[0]
+    
+    strs.sort()
+    p = ""
+    for x, y in zip(strs[0], strs[-1]):
+        if x == y: p+=x
+        else: break
+    return r
+
+
+input = ["flower","flow","flight"]
+input = ["dog","racecar","car"]
+input = ["aca","cba"]
+print(longestCommonPrefix(input))
---
+++ 16_3Sum_Closest.py
@@ -0,0 +1,80 @@
+
+
+def threeSumClosest(nums, target):
+    from itertools import combinations 
+    list_of_list = []
+    comb = combinations(nums, 3) 
+    for i in list(comb): 
+        list_of_list.append(list(i))
+
+    # print(list_of_list)
+
+    diff = []
+    sum_of_list = []
+    for item in list_of_list:
+        diff.append(abs(sum(item) - target))
+        sum_of_list.append(sum(item))
+
+    index = diff.index(min(diff))
+    ans = sum_of_list[index]
+
+    return ans
+
+# ref to 15 other solution and modify
+def threeSumClosest(nums, target):
+    list_of_list = []
+    nums.sort()
+    for i in range(len(nums)-2):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        l, r = i+1, len(nums)-1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+
+            if s < target:
+                list_of_list.append([nums[i], nums[l], nums[r]])
+                l +=1 
+            elif s > target:
+                list_of_list.append([nums[i], nums[l], nums[r]])
+                r -= 1
+            else:
+                list_of_list.append([nums[i], nums[l], nums[r]])
+                l += 1; r -= 1
+
+    diff = []
+    sum_of_list = []
+    for item in list_of_list:
+        diff.append(abs(sum(item) - target))
+        sum_of_list.append(sum(item))
+
+    index = diff.index(min(diff))
+    ans = sum_of_list[index]
+
+    return ans
+
+# others solution, faster
+def threeSumClosest(num, target):
+        
+    num.sort()
+    result = num[0] + num[1] + num[2]
+    for i in range(len(num) - 2):
+        j, k = i+1, len(num) - 1
+        while j < k:
+            sum = num[i] + num[j] + num[k]
+            if sum == target:
+                return sum
+            
+            if abs(sum - target) < abs(result - target):
+                result = sum
+            
+            if sum < target:
+                j += 1
+            elif sum > target:
+                k -= 1
+        
+    return result
+
+
+nums = [-1, 2, 1, -4]
+target = 1
+print(threeSumClosest(nums, target))
